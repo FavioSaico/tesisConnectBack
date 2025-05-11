2affe8b8c0474e0e2c2e5b672fc734cc0d7762d4
FavioGenaro <faviogenarosaico@gmail.com>
2025-05-11 16:50:17 -0500

    Configurar cors y variable de entorno port

--- api-recomendacion2/main.py
+++ api-recomendacion2/main.py
@@ -1,9 +1,20 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.infrastructure.db import Base, engine
 from src.presentation.controllers.RecomendacionController import router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Agregamos el middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
