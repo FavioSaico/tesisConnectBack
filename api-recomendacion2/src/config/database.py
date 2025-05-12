324f31f8eda0bc2523a58981aa782840745af3b3
FavioGenaro <faviogenarosaico@gmail.com>
2025-05-11 22:45:40 -0500

    modificando configuración de variables de entorno

--- api-recomendacion2/src/config/database.py
+++ api-recomendacion2/src/config/database.py
@@ -1,19 +1,25 @@
-from dotenv import load_dotenv
-load_dotenv()
-
+#from dotenv import load_dotenv
+#load_dotenv()
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
+# Solo carga .env si no estás en producción
+if os.getenv("ENV") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
 
 class Settings(BaseSettings):
     # Configuración para usar dotenv (comentada porque usaremos valores fijos)
     # model_config = SettingsConfigDict(env_file=".env")
+    
 
     MYSQL_HOST: str = Field(..., env="MYSQL_HOST")
     MYSQL_PORT: int = Field(..., env="MYSQL_PORT")
     MYSQL_USER: str = Field(..., env="MYSQL_USER")
     MYSQL_PASS: str = Field(..., env="MYSQL_PASS")
     MYSQL_DB_NAME: str = Field(..., env="MYSQL_DB_NAME")
+    ENV: str = Field(..., env="ENV")
 
     PORT: int = Field(..., env="PORT")
 
