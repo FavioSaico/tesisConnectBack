293d5ae7eeb6e379b4b0ca66d9a79ede6c9d7880
Seb2103 <jhamir.cespedes@unmsm.edu.pe>
2025-05-26 13:36:52 -0500

    gRPC usuario server DB

--- api-usuario/grpc_server.py
+++ api-usuario/grpc_server.py
@@ -8,17 +8,34 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'p
 import user_pb2
 import user_pb2_grpc
 
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+pymysql://root:admin@localhost:3306/tesisconnect"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'Usuarios'
+    id = Column(Integer, primary_key=True, index=True)
+    nombre_completo = Column(String(100))
 
 class UserService(user_pb2_grpc.UserServiceServicer):
+    def __init__(self):
+        self.db = SessionLocal()
+
     def GetUser(self, request, context):
-        fake_users = {
-            "1": "Ana Torres",
-            "2": "Luis Paredes",
-            "3": "Valeria Díaz"
-        }
-        print(f"Recibido user_id: {request.user_id}")
-        name = fake_users.get(request.user_id, "Desconocido")
-        return user_pb2.UserResponse(name=name)
+        user_id = int(request.user_id)
+        user = self.db.query(Usuario).filter(Usuario.id == user_id).first()
+        if user:
+            return user_pb2.UserResponse(name=user.nombre_completo)
+        else:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details("Usuario no encontrado")
+            return user_pb2.UserResponse()
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
